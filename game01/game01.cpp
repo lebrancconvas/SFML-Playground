435a4ff7bc5d238622320087f876566c4276f4db
lebrancconvas <lebrancconvas@gmail.com>
2022-02-02 00:33:31 +0700

    ✨ Init new game window.

---
+++ game01/game01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+	std::cout << "Hello Game 01." << std::endl;
+ 	return 0;
+}
